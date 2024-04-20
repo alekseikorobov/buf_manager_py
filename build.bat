7ab8131d4d0c373316c409d24a2d477b8c2e5bf3
alekseikorobov <aleksei_korobov@mail.ru>
2024-04-20 21:52:35 +0300

    not one file build

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 RMDIR dist_windows /S /Q
 RMDIR build /S /Q
 DEL main.spec
-pyinstaller main.py --noconsole --onefile
+pyinstaller main.py --noconsole
 
 MOVE dist dist_windows
-COPY params.json dist_windows/params.json
+COPY params.json dist_windows\params.json
